c49a911b6a77e92729c527ec3a3baa885062a9b2
pra9989 <36298255+pra9989@users.noreply.github.com>
2026-09-20 22:57:43 +0530

    Add EKS node group resource configuration

---
+++ modules/eks/nodegroups.tf
@@ -0,0 +1,28 @@
+resource "aws_eks_node_group" "this" {
+  for_each = var.cluster_type == "managed" ? var.node_groups : {}
+
+  cluster_name    = aws_eks_cluster.this.name
+  node_group_name = "${var.cluster_name}-${each.key}"
+  node_role_arn   = aws_iam_role.node[0].arn
+
+  subnet_ids = module.vpc[0].private_subnets
+
+  instance_types = each.value.instance_types
+  capacity_type  = each.value.capacity_type
+  disk_size      = each.value.disk_size
+
+  scaling_config {
+    desired_size = each.value.desired_size
+    min_size     = each.value.min_size
+    max_size     = each.value.max_size
+  }
+
+  labels = each.value.labels
+  tags   = var.tags
+
+  depends_on = [
+    aws_iam_role_policy_attachment.node_worker,
+    aws_iam_role_policy_attachment.node_cni,
+    aws_iam_role_policy_attachment.node_ecr
+  ]
+}
